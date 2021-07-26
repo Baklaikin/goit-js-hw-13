4b3aafc04613ce81839c332dccef403e825efdfe
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-07-26 20:41:13 +0300

    added smooth scroll and lightbox

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
-// import Handlebars from 'handlebars';
 import cardTmt from './templates/cardTmt.hbs';
 import axios from 'axios';
 import ApiService from './api';
-// import SimpleLightbox from '../node_modules/simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
     galleryForm: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('.load-more'),
 };
-// const SimpleLightBox = new SimpleLightbox(('.gallery a'));
+const lightbox = new SimpleLightbox('.photo-card a');
+
+// Создаю экземпляр класса
 const Api = new ApiService();
 
+// При нажатии на кнопку поиска
 function onSearchInput(e) {
     e.preventDefault();
     resetSearchWord();
@@ -32,25 +34,41 @@ function onSearchInput(e) {
     });
 }
 
+//Функция создания разметки
 function createMarkup(data) {  
     refs.galleryForm.insertAdjacentHTML('beforeend', cardTmt(data.hits));
+    lightbox.refresh();
 }
+
+// При нажатии на кнопку "Загрузить еще"
 function onLoadMoreBtnClick() {
     Api.fetchPictures().then(data => {
         createMarkup(data);
+        lightbox.refresh();
+        smoothScroll();
         return data
     }).then(data => {
         if(data.hits.length < 40){
            Notiflix.Notify.info(`We're sorry, but you've reached the end of search results`);
            refs.loadMoreBtn.classList.remove('is-visible');
        }
-    });   
+    });
+
+
 }
 
 refs.searchForm.addEventListener('submit', onSearchInput)
 refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick)
 
-
+// Функция очистки галереи
 function resetSearchWord() {
     refs.galleryForm.innerHTML = '';
+}
+
+function smoothScroll() {
+const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
+   window.scrollBy({
+   top: cardHeight * 1.5,
+   behavior: 'smooth',
+   });
 }
